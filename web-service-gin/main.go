b4b5acb587a29bcc7bd9f6e4dd43121c41bb859a
Phil Lisak <phil.lisak@gmail.com>
2022-01-06 00:32:34 -0700

    endpoints up and running

--- web-service-gin/main.go
+++ web-service-gin/main.go
@@ -1,41 +1,34 @@
 package main
 
 import (
-	"net/http"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
-
-// produce represents data about a piece of produce
-type produce struct {
-	ID int `json:"id"`
+type Produce struct {
 	Name string `json:"name"`
 	Produce_Code string `json:"code"`
 	Unit_Price float64 `json:"price"`
 }
 
-// produce sliced to seed list data
-var prodList = []produce{
+var prodList = []Produce{
 	{
-		ID: 1,
 		Produce_Code: "A12T-4GH7-QPL9-3N4M",
 		Name: "Lettuce",
 		Unit_Price: 3.46,
 	},
 	{
-		ID: 2,
 		Produce_Code: "E5T6-9UI3-TH15-QR88",
 		Name: "Peach",
 		Unit_Price: 2.99,
 	},
 	{
-		ID: 3,
 		Produce_Code: "YRT6-72AS-K736-L4AR",
 		Name: "Green Pepper",
 		Unit_Price: 0.79,
 	},
 	{
-		ID: 4,
 		Produce_Code: "TQ4C-VV6T-75ZX-1RMR",
 		Name: "Gala Apple",
 		Unit_Price: 3.59,
@@ -43,28 +36,57 @@ var prodList = []produce{
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/produce", getProduce)
-	router.POST("/produce", postItems)
+	r := gin.Default()
 
-	router.Run("localhost:8080")
+	prodRoutes := r.Group("/produce")
+	{
+		prodRoutes.GET("/", getHandler)
+		prodRoutes.POST("/", createItem)
+		prodRoutes.DELETE("/:id", deleteItem)
+	}	
+
+	if err := r.Run("localhost:8080"); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
-// getProduce responds with list of all produce in JSON
-func getProduce(c *gin.Context) {
-	c.JSON(http.StatusOK, prodList)
+func getHandler(c *gin.Context) {
+	c.JSON(200, prodList)
 }
 
-//postItems adds produce from JSON received in req body
-func postItems(c *gin.Context) {
-	var newItem produce
-
-	// Call BindJSON to bind the received JSON to newItem
-	if err := c.BindJSON(&newItem); err != nil {
+func createItem(c *gin.Context) {
+	var reqBody Produce
+	if err := c.ShouldBindJSON(&reqBody); err != nil {
+		c.JSON(422, gin.H{
+			"error": true,
+			"message": "invalid request body",
+		})
 		return
 	}
 
-	// Add new item to the slice
-	prodList = append(prodList, newItem)
-	c.JSON(http.StatusCreated, newItem)
+	reqBody.Produce_Code = uuid.New().String()
+
+	prodList = append(prodList, reqBody)
+	c.JSON(200, gin.H{
+		"error": false,
+	})
+}
+
+func deleteItem(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, p := range prodList {
+		if p.Produce_Code == id {
+			prodList = append(prodList[:i], prodList[i + 1:]...)
+
+			c.JSON(200, gin.H{
+				"error": false,
+			})
+			return
+		}
+	}
+	c.JSON(404, gin.H{
+		"error": true,
+		"message": "invalid id",
+	})
 }
